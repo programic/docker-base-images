f3012b2c98456f3a5e9f1f548a376e066b2d22be
Remco Wolvenne <r.wolvenne@programic.com>
2021-12-19 10:43:47 +0100

    Typo.

--- php/7.4-latex-bullseye/build.sh
+++ php/7.4-latex-bullseye/build.sh
@@ -3,9 +3,9 @@
 docker login
 
 # Building FPM version
-docker build --build-arg type=fpm -t programic/php:7.4-fpm-latex-bullseye .
+docker build --build-arg TYPE=fpm -t programic/php:7.4-fpm-latex-bullseye .
 docker push programic/php:7.4-fpm-latex-bullseye
 
 # Building CLI version
-docker build --build-arg type=cli -t programic/php:7.4-cli-latex-bullseye .
+docker build --build-arg TYPE=cli -t programic/php:7.4-cli-latex-bullseye .
 docker push programic/php:7.4-cli-latex-bullseye
