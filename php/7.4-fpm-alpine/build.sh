befb25ed323ecddf2fed90091ca0873dd03103ca
Remco Wolvenne <r.wolvenne@programic.com>
2022-04-24 16:47:12 +0200

    Set platform to linux/amd64 so image can be build on Apple M1.

--- php/7.4-fpm-alpine/build.sh
+++ php/7.4-fpm-alpine/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker login
-docker build -t programic/php:7.4-fpm-alpine .
+docker build --platform linux/amd64 -t programic/php:7.4-fpm-alpine .
 docker push programic/php:7.4-fpm-alpine
