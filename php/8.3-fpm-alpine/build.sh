d5d4a40a9049215ae6130f8cef0d7ee32b8965dc
Rick Bongers <r.bongers@programic.com>
2024-03-27 17:41:31 +0100

    php 8.3 build

---
+++ php/8.3-fpm-alpine/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker login
+docker buildx create --name docker-multiplatform --bootstrap --use
+docker buildx build --push --platform linux/amd64,linux/arm64,linux/arm/v8 -t programic/php:8.3-fpm-alpine .
